e8375b8b77a3d926de318586ee972d59a0948c9c
Yanai1005 <108969330+Yanai1005@users.noreply.github.com>
2022-09-09 20:32:06 +0900

    develop

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -63,7 +63,8 @@ class TweetController extends Controller
      */
     public function show($id)
     {
-        //
+      $tweet = Tweet::find($id);
+      return view('tweet.show', compact('tweet'));
     }
 
     /**
@@ -74,7 +75,8 @@ class TweetController extends Controller
      */
     public function edit($id)
     {
-        //
+      $tweet = Tweet::find($id);
+      return view('tweet.edit', compact('tweet'));
     }
 
     /**
@@ -85,8 +87,22 @@ class TweetController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
-        //
+    { 
+      //バリデーション
+      $validator = Validator::make($request->all(), [
+        'tweet' => 'required | max:191',
+        'description' => 'required',
+      ]);
+      //バリデーション:エラー
+      if ($validator->fails()) {
+        return redirect()
+          ->route('tweet.edit', $id)
+          ->withInput()
+          ->withErrors($validator);
+      }
+       //データ更新処理
+       $result = Tweet::find($id)->update($request->all());
+       return redirect()->route('tweet.index');
     }
 
     /**
@@ -97,6 +113,7 @@ class TweetController extends Controller
      */
     public function destroy($id)
     {
-        //
+      $result = Tweet::find($id)->delete();
+      return redirect()->route('tweet.index');
     }
 }
