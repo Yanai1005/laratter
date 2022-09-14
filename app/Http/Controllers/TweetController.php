fa46ff866d82c1ae3076c9a840afe1f4e1d87a91
Yanai1005 <108969330+Yanai1005@users.noreply.github.com>
2022-09-14 10:12:47 +0900

    develop

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use App\Models\Tweet;
+use Auth;
+use App\Models\User;
+
 class TweetController extends Controller
 {
     /**
@@ -50,7 +53,8 @@ class TweetController extends Controller
       }
        // create()は最初から用意されている関数
        // 戻り値は挿入されたレコードの情報
-       $result = Tweet::create($request->all());
+       $data = $request->merge(['user_id' => Auth::user()->id])->all();
+       $result = Tweet::create($data);
        // ルーティング「todo.index」にリクエスト送信（一覧ページに移動）
        return redirect()->route('tweet.index');
     }
@@ -115,5 +119,16 @@ class TweetController extends Controller
     {
       $result = Tweet::find($id)->delete();
       return redirect()->route('tweet.index');
+    }
+
+    public function mydata()
+    {
+      // Userモデルに定義したリレーションを使用してデータを取得する．
+      $tweets = User::query()
+        ->find(Auth::user()->id)
+        ->userTweets()
+        ->orderBy('created_at','desc')
+        ->get();
+      return view('tweet.index', compact('tweets'));
     }
 }
